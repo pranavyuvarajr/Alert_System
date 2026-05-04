47913741edfc6cf026962aa9c6f3e26cb4202fe3
pranavyuvarajr <pranavyuvarajr@gmail.com>
2026-05-04 17:30:29 +0530

    logs understanding

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
+
+@RestController
+@RequestMapping("/api")
+@Slf4j
+
+class temp {
+    @GetMapping
+    public String hello() {
+        log.info("This is just a {}", "Info");
+        return "Hello World";
+    }
+}
